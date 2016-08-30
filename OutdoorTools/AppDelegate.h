e6689da33fe5d143c877a5999ffe75442db0862e
aihua <liuaihuajm@126.com>
2016-08-30 09:53:45 +0800

    Initial Commit

---
+++ OutdoorTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OutdoorTools
+//
+//  Created by aaaa on 16/8/30.
+//  Copyright © 2016年 aihua Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OutdoorTools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OutdoorTools
+//
+//  Created by aaaa on 16/8/30.
+//  Copyright © 2016年 aihua Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
